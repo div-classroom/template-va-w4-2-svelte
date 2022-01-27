da1c9e7c92cda6d55de1db0562a74ff51186f2b2
Minsuk Kahng <minsukkahng@users.noreply.github.com>
2022-01-26 20:58:43 -0800

    Add a minimum template

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	export const myName = "REPLACE_WITH_YOUR_NAME";
+</script>
+
+<main>
+	<h1>In-Class Acitivity by {myName}!</h1>
+
+	<svg id="visualization-activity">
+
+
+	</svg>
+</main>
+
+<style>
+	h1 {
+		font-size: 1.5rem;
+	}
+</style>
